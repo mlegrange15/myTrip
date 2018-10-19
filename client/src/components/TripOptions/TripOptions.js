c84b24f4b9b967eee9b362439c9a5a8279f9405f
Michael LeGrange <mlegrange15@att.net>
2018-10-19 18:08:51 -0500

    updated size on trip options

--- client/src/components/TripOptions/TripOptions.js
+++ client/src/components/TripOptions/TripOptions.js
@@ -1,5 +1,7 @@
 import React, { Component } from "react";
 import {
+  Container,
+  Row,
   Col,
   Card,
   CardImg,
@@ -13,16 +15,58 @@ class TripOptions extends Component {
   state = {};
   render() {
     return (
-      <Card inverse>
-        <CardImg width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97270&w=318&h=270&bg=333333&txtclr=666666" alt="Card image cap" />
-        <CardImgOverlay>
-          <CardTitle>Card Title</CardTitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <CardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </CardText>
-        </CardImgOverlay>
-      </Card>
+      <Container className="mb-3">
+        <Row>
+          <Col sm="3">
+            <Card inverse>
+              <CardImg
+                width="100%"
+                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97270&w=318&h=150&bg=333333&txtclr=666666"
+                alt="Card image cap"
+              />
+              <CardImgOverlay>
+                <CardTitle>Hotels</CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </Col>
+          <Col sm="3">
+            <Card inverse>
+              <CardImg
+                width="100%"
+                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97150&w=318&h=150&bg=333333&txtclr=666666"
+                alt="Card image cap"
+              />
+              <CardImgOverlay>
+                <CardTitle>Events</CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </Col>
+          <Col sm="3">
+            <Card inverse>
+              <CardImg
+                width="100%"
+                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97150&w=318&h=150&bg=333333&txtclr=666666"
+                alt="Card image cap"
+              />
+              <CardImgOverlay>
+                <CardTitle>Food</CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </Col>
+          <Col sm="3">
+            <Card inverse>
+              <CardImg
+                width="100%"
+                src="https://placeholdit.imgix.net/~text?txtsize=33&txt=318%C3%97150&w=318&h=150&bg=333333&txtclr=666666"
+                alt="Card image cap"
+              />
+              <CardImgOverlay>
+                <CardTitle>Tours</CardTitle>
+              </CardImgOverlay>
+            </Card>
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
